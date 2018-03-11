5d0709fd13478a3ff84da6010a1ae00174342b19
David <david.bauske@googlemail.com>
2018-03-11 03:48:57 -0700

    Implemented drive-core (low-level driving function, steering, throttle and braking)

--- drive-core/src/main.rs
+++ drive-core/src/main.rs
@@ -1,15 +1,42 @@
 extern crate i2cdev;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate bincode;
+extern crate bufstream;
+
 mod error;
 mod pwm_driver;
+mod message_type;
 
 use pwm_driver::*;
+use message_type::MessageType;
+
+use std::net::*;
+use std::io;
+use bufstream::BufStream;
+use std::time::Duration;
+use bincode::deserialize_from;
 
 const PWM_DRIVER_ADDRESS: u16 = 0x40;
 const PWM_FREQUENCY: f32 = 50f32;
 const I2C_DEVICE_PATH: &str = "/dev/i2c-1";
 
+fn accept_connection(listener: &TcpListener) -> io::Result<BufStream<TcpStream>> {
+  let (socket, addr) = listener.accept()?;
+  println!("Client connected from {}", addr);
+
+  // We don't want to block longer than 100ms. If we go above this threshold, then we consider
+  // the client unresponsive and cut the power to the motor.
+  socket.set_read_timeout(Option::Some(Duration::from_millis(100)))?;
+
+  return Ok(BufStream::new(socket));
+}
+
 fn main() {
+  // Create and initialize the PWM driver. It's constructor
+  // will set both servos to their respective neutral position.
   let mut device = PwmDriver::new(I2C_DEVICE_PATH, PwmArgs {
     address: PWM_DRIVER_ADDRESS,
     freq: PWM_FREQUENCY,
@@ -23,6 +50,48 @@ fn main() {
     }
   }).unwrap();
 
-  device.set_steering(0f32).unwrap();
-  device.set_throttle(0f32).unwrap();
+  // Set up a listening network socket to receive new connections on.
+  // The drive-core service is controlled by such network
+  // connections only (they may come from localhost).
+  let listener = TcpListener::bind("0.0.0.0:41330").unwrap();
+
+  loop {
+    let mut socket = match accept_connection(&listener) {
+      Ok(socket) => socket,
+      Err(e) => {
+        println!("Failed to accept incoming socket: {:?}", e);
+        continue
+      }
+    };
+
+    loop {
+      // Repeatedly read messages from the socket. If the socket fails,
+      // then we'll drop the client and wait for a new one
+      let msg: MessageType = match deserialize_from(&mut socket) {
+        Ok(msg) => msg,
+        Err(e) => {
+          match *e {
+            bincode::ErrorKind::Io(ref e) if e.kind() == io::ErrorKind::TimedOut
+                || e.kind() == io::ErrorKind::WouldBlock => {
+              // Timeout => Disable power
+              device.set_steering(0f32).unwrap();
+              device.set_throttle(0f32).unwrap();
+              continue;
+            }
+            e => {
+              println!("Reading from socket failed. Dropping client. {:?}", e);
+              break;
+            }
+          }
+        }
+      };
+
+      // Handle the message
+      match msg {
+        MessageType::SetSteering(val) => device.set_steering(val),
+        MessageType::SetThrottle(val) => device.set_throttle(val),
+      }.unwrap();
+    }
+  }
 }
+
---
+++ drive-core/src/message_type.rs
@@ -0,0 +1,68 @@
+// Enum of the messages this service accepts
+#[derive(Debug, Serialize, Deserialize)]
+pub enum MessageType {
+  SetSteering(f32),
+  SetThrottle(f32)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use bincode::{ serialize, deserialize };
+
+  #[test]
+  fn serialize_set_steering() {
+    let expected = -0.987f32;
+    let vec = serialize(&MessageType::SetSteering(expected)).unwrap();
+    assert_eq!(vec.len(), 8);
+
+    let msg = deserialize(&vec[..]).unwrap();
+    match msg {
+      MessageType::SetSteering(actual) => assert_eq!(actual, expected),
+      _ => panic!("Deserialized the wrong value")
+    }
+  }
+
+  #[test]
+  fn serialize_set_throttle() {
+    let value = 132.321f32;
+    let vec = serialize(&MessageType::SetThrottle(value)).unwrap();
+    assert_eq!(vec.len(), 8);
+
+    let msg = deserialize(&vec[..]).unwrap();
+    match msg {
+      MessageType::SetThrottle(actual) => assert_eq!(actual, value),
+      _ => panic!("Deserialized the wrong value")
+    }
+  }
+
+  #[test]
+  fn deserialize_set_steering() {
+    let raw = [0, 0, 0, 0, 0xd5, 0xd0, 0x4d, 0x44];
+    let msg = deserialize(&raw).unwrap();
+    match msg {
+      MessageType::SetSteering(actual) => assert_eq!(actual, 823.263f32),
+      _ => panic!("Deserialized the wrong value")
+    }
+  }
+
+  #[test]
+  fn deserialize_set_throttle() {
+    let raw = [1, 0, 0, 0, 0x87, 0x16, 0x39, 0xbf];
+    let msg = deserialize(&raw).unwrap();
+    match msg {
+      MessageType::SetThrottle(actual) => assert_eq!(actual, -0.723f32),
+      _ => panic!("Deserialized the wrong value")
+    }
+  }
+
+  #[test]
+  fn deserialize_additional_bytes() {
+    let raw = [1, 0, 0, 0, 0x42, 0x60, 0x05, 0x3f, 1, 0, 0, 0];
+    let msg = deserialize(&raw).unwrap();
+    match msg {
+      MessageType::SetThrottle(actual) => assert_eq!(actual, 0.521f32),
+      _ => panic!("Deserialized the wrong value")
+    }
+  }
+}
