35a30733c9b19e9b961eba874bf12d0062987599
Arvius <arvius@web.de>
2018-04-10 21:26:38 +0200

    Proper handling of simultanious braking and accelerating using a gamepad (solves #2).

--- drive-remote/src/gamepad_device.rs
+++ drive-remote/src/gamepad_device.rs
@@ -17,6 +17,7 @@ impl GamepadDevice {
 
 impl InputDevice for GamepadDevice {
   fn poll(&mut self, inputs: &mut Inputs) {
+    let (mut brake, mut throttle) = (0f32, 0f32);
     for ev in &mut self.device {
       use joy::Event::*;
       match ev {
@@ -27,11 +28,11 @@ impl InputDevice for GamepadDevice {
 //              println!("axis 0: {}\r", inputs.steering);
             }
             4 => {
-              inputs.throttle = ((value as i32 + 0x7FFF_i32) as f32) / (0xFFFF as f32);
+              throttle = ((value as i32 + 0x7FFF_i32) as f32) / (0xFFFF as f32);
 //              println!("axis 4: {}\r", inputs.throttle);
             }
             5 => {
-              inputs.throttle = -((value as i32 + 0x7FFF_i32) as f32) / (0xFFFF as f32);
+              brake = -((value as i32 + 0x7FFF_i32) as f32) / (0xFFFF as f32);
 //              println!("axis 5: {}\r", inputs.throttle);
             }
             _ => {}
@@ -40,5 +41,10 @@ impl InputDevice for GamepadDevice {
         Button(_, _) => {}
       }
     }
+    inputs.throttle = if brake.abs() > 0.05 {
+      brake
+    } else {
+      throttle + brake
+    };
   }
 }
